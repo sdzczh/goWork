9d4aff36aa2b979a9df290d19152d511888847df
sdzczh <zgsdzczh@163.com>
2018-10-10 15:39:11 +0800

    Test Mysql

--- TestMysql.go
+++ TestMysql.go
@@ -11,12 +11,22 @@ func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/yibi")
 	checkErr(err)
 
-	err = db.Ping()
-	if err == nil {
-		fmt.Println("ping is ok")
+	/*查询*/
+	hql := "select * from yibi.user where id = 1"
+	rows, err := db.Query(hql)
+	fmt.Println(rows)
+	defer rows.Close()
+	/*增加*/
+	_, err = db.Exec("insert into yibi.user(id, account) values(null, '20')")
+	/*删除*/
+	_, err = db.Exec("delete from yibi.user where id = 38")
+	for rows.Next() {
+		var fileId1 string
+		var fileId2 string
+		err := rows.Scan(&fileId1, &fileId2)
+		fmt.Println(fileId1, fileId2)
+		checkErr(err)
 	}
-	checkErr(err)
-	defer db.Close()
 }
 func checkErr(err error) {
 	if err != nil {
